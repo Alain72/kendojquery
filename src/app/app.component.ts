4e69561194121b5ce8f0f48268e78f013bf96e82
alain van Gaalen <alain.vangaalen@subsea7.com>
2021-09-15 13:36:42 +0200

    first mcoo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import '@progress/kendo-ui';
+
+declare var kendo: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'kendojquery';
+export class AppComponent implements AfterViewInit, OnDestroy  {
+  @ViewChild('editor', { static: true }) editorEl: ElementRef;
+    
+
+    public value: string;
+
+    constructor(private hostEl: ElementRef) { 
+     
+    }
+
+    ngAfterViewInit() {
+        kendo.jQuery(this.editorEl.nativeElement).kendoEditor({
+            // Define Editor's options here
+            // Use @Input() properties to expose Editor configuration
+            resizable: {
+                content: true,
+                toolbar: true
+            },
+            change: (args) => {
+                this.value = args.sender.value();
+            }
+        });
+    }
+
+    ngOnDestroy(): void {
+        kendo.destroy(this.hostEl.nativeElement);
+    }
 }
